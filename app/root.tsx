f0a2c9460034b13b32cdbec8bdd71d831ba6d749
Colby Thomas <coloradocolby@gmail.com>
2022-06-12 21:01:16 -0600

    tailwindcss configuration

--- app/root.tsx
+++ app/root.tsx
@@ -7,12 +7,17 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from "./styles/build.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
 
 export default function App() {
   return (
@@ -21,7 +26,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-[#121212] text-zinc-100">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
