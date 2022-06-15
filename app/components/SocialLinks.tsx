47487e67e2d8ae1fa2d435c294c1ff20937ad136
Colby Thomas <coloradocolby@gmail.com>
2022-06-14 20:57:35 -0600

    a11y

--- app/components/SocialLinks.tsx
+++ app/components/SocialLinks.tsx
@@ -9,33 +9,39 @@ import {
 const socialLinks = [
   {
     url: 'https://github.com/thatvegandev',
+    label: 'github',
     Icon: GithubIcon,
   },
   {
     url: 'https://twitter.com/thatvegandev',
+    label: 'twitter',
     Icon: TwitterIcon,
   },
   {
     url: 'https://instagram.com/thatvegandev',
+    label: 'instagram',
     Icon: InstagramIcon,
   },
   {
     url: 'https://www.youtube.com/c/thatvegandev',
+    label: 'youtube',
     Icon: YoutubeIcon,
   },
   {
     url: 'https://linkedin.com/in/thatvegandev',
+    label: 'linkedin',
     Icon: LinkedinIcon,
   },
 ]
 export const SocialLinks = () => (
   <div className="flex justify-center space-x-6">
-    {socialLinks.map(({ url, Icon }) => (
+    {socialLinks.map(({ url, label, Icon }) => (
       <a
         href={url}
         target="_blank"
         rel="noopener noreferrer"
         key={url}
+        aria-label={`follow me on ${label}`}
         className="rounded-md focus:outline-none focus:ring-2 focus:ring-violet-400"
       >
         <Icon
